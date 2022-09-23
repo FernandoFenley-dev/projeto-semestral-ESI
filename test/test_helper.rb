65c7f409e5c333c9d177f5b1e1901b6e04cb44dc
Fernando Fenley <fernandojfenley@gmail.com>
2022-09-23 15:10:55 -0300

    configs2

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,20 @@
 require 'simplecov'
+
+SimpleCov.start 'rails' do
+  if ENV['CI']
+    require 'simplecov-lcov'
+
+    SimpleCov::Formatter::LcovFormatter.config do |c|
+      c.report_with_single_file = true
+      c.single_report_path = 'coverage/lcov.info'
+    end
+
+    formatter SimpleCov::Formatter::LcovFormatter
+  end
+
+  add_filter %w[version.rb initializer.rb]
+end
+require 'simplecov'
 require 'simplecov_json_formatter'
 SimpleCov.formatter = SimpleCov::Formatter::JSONFormatter
 SimpleCov.start 'rails'
