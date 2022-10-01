99de82c2db66573a65239ddac34b606dfdf67486
Felipe Oliveira <felipeoes@usp.br>
2022-10-01 16:11:52 -0300

    fix: add coveralls files

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,23 +1,5 @@
 require 'simplecov'
-
-SimpleCov.start 'rails' do
-  if ENV['CI']
-    require 'simplecov-lcov'
-
-    SimpleCov::Formatter::LcovFormatter.config do |c|
-      c.report_with_single_file = true
-      c.single_report_path = 'coverage/lcov.info'
-    end
-
-    formatter SimpleCov::Formatter::LcovFormatter
-  end
-
-  add_filter %w[version.rb initializer.rb]
-end
-require 'simplecov'
-require 'simplecov_json_formatter'
-SimpleCov.formatter = SimpleCov::Formatter::JSONFormatter
-SimpleCov.start 'rails'
+require 'simplecov-lcov'
 
 ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"
