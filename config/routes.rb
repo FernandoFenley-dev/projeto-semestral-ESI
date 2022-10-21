c43a75edb66b6bca71166edf669ad47041c8275d
Augusto Xavier Neto <61883585+XavierNeto@users.noreply.github.com>
2022-10-21 16:43:17 -0300

    Revert "Formulário de cadastro - cliente"

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :clientes
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get "about", to:"about#index"
 
   root to:"main#index"
   # Defines the root path route ("/")
