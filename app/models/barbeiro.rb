8abde1560d3bb9512952747736282b1f141da28a
Guilherme Jimenes <guilherme.jimenes@usp.br>
2022-10-24 21:33:33 -0300

    ajustando test cucumber

--- app/models/barbeiro.rb
+++ app/models/barbeiro.rb
@@ -1,8 +1,8 @@
 class Barbeiro < ApplicationRecord
-    validates :nome, presence: { message: "Digite seu nome! - Barbeiro" }
-    validates :email, uniqueness: true, presence: { message: "Digite seu email! - Barbeiro" },format: { with: URI::MailTo::EMAIL_REGEXP,
-        message: "Email inválido! - Barbeiro" }
-    validates :senha, presence: { message: "Crie uma senha! - Barbeiro" }
+  validates :nome, presence: { message: "Digite seu nome! - Barbeiro" }
+  validates :email, uniqueness: true, presence: { message: "Digite seu email! - Barbeiro" },format: { with: URI::MailTo::EMAIL_REGEXP,
+    message: "Email inválido! - Barbeiro" }
+  validates :senha, presence: { message: "Crie uma senha! - Barbeiro" }
   
-  end
+end
   
