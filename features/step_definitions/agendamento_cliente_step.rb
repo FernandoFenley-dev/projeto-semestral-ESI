bd4423d3b2eb924a0263a8115cd456980f265078
Fernando Fenley <fernandojfenley@gmail.com>
2022-11-08 11:58:47 -0300

    testes

---
+++ features/step_definitions/agendamento_cliente_step.rb
@@ -0,0 +1,16 @@
+Dado('que estou na página de agendamento de serviço') do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Quando('seleciono o ano {string}') do |string|
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Quando('clico em Agendar') do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Então('o Agendamento deve ter sido salvo no banco de dados') do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
