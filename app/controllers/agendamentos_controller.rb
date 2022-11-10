a84fb0807b9c47eacd414ab2ff70097148f2e5e7
FernandoFenley-dev <57076081+FernandoFenley-dev@users.noreply.github.com>
2022-11-10 11:57:16 -0300

    Update agendamentos_controller.rb

--- app/controllers/agendamentos_controller.rb
+++ app/controllers/agendamentos_controller.rb
@@ -34,11 +34,19 @@ class AgendamentosController < ApplicationController
     end
 
     def barbeador
-        @agendamentos = Agendamento.where("barbeiro_id = :barbeiro_id 
-            AND date(agendamentos.dataAgendamento) in (:data)",
-            barbeiro_id: params[:barbeiro_id],
-            data: params[:data]
-        )
+        if Rails.env.development? || Rails.env.test?
+            @agendamentos = Agendamento.where("barbeiro_id = :barbeiro_id 
+                AND date(dataAgendamento) in (:data)",
+                barbeiro_id: params[:barbeiro_id],
+                data: params[:data]
+            )
+        else
+            @agendamentos = Agendamento.where("barbeiro_id = :barbeiro_id 
+                AND to_char(dataAgendamento, 'yyyy-mm-dd') in (:data)",
+                barbeiro_id: params[:barbeiro_id],
+                data: params[:data]
+            )
+        end
         @agendamentos = @agendamentos.uniq.sort_by! {|obj| obj.dataAgendamento  unless obj.blank?}
     end
 
