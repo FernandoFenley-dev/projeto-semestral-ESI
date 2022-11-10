d0f0f04aa70f82e684d5730bf732a8d21221b491
Fernando Fenley <fernandojfenley@gmail.com>
2022-11-10 10:21:02 -0300

    detalhes-deploy

--- app/controllers/agendamentos_controller.rb
+++ app/controllers/agendamentos_controller.rb
@@ -35,7 +35,7 @@ class AgendamentosController < ApplicationController
 
     def barbeador
         @agendamentos = Agendamento.where("barbeiro_id = :barbeiro_id 
-            AND date(dataAgendamento) in (:data)",
+            AND date(agendamentos.dataAgendamento) in (:data)",
             barbeiro_id: params[:barbeiro_id],
             data: params[:data]
         )
